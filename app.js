3a02dbcc3d3fb1b0566a0abda4ef70b02200db54
pinjin1201 <knowledge84121@gmail.com>
2021-07-18 10:30:56 +0800

    modify index.handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const Restaurant = require('./models/restaurant')
 
 const app = express()
 const port = 3000
@@ -9,6 +10,8 @@ mongoose.connect('mongodb://localhost/restaurant', { useNewUrlParser: true, useU
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
 
 const db = mongoose.connection
 
@@ -20,8 +23,28 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+// 首頁路由
 app.get('/', (req, res) => {
-  res.render('index')
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.error(error))
+})
+
+// search 路由
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.trim().toLowerCase()
+
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      restaurants = restaurants.filter(restaurant =>
+        restaurant.name.toLowerCase().includes(keyword) ||
+        restaurant.category.toLowerCase().includes(keyword)
+      )
+      res.render('index', { restaurants })
+    })
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
