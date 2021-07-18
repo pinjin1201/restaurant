cd73281d539ace926130f0634056de09517b19ab
pinjin1201 <knowledge84121@gmail.com>
2021-07-18 10:37:28 +0800

    add create restaurant function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
@@ -12,6 +13,7 @@ app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 const db = mongoose.connection
 
@@ -44,6 +46,20 @@ app.get('/search', (req, res) => {
       )
       res.render('index', { restaurants })
     })
+    .catch(error => console.log(error))
+})
+
+// 新增資料路由
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+// 新建的資料傳入資料庫
+app.post('/restaurants', (req, res) => {
+  const { name, name_en, category, image, location, phone, google_map, rating, description } = req.body
+
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
